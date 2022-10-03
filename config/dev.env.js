b6f11b296bd88ae462cbbe3797c909697eeaaf23
yangtian <yangtian9999@163.com>
2022-10-03 13:33:07 +0800

    fix base api

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"mall-admin.mall-k8s.com"'
+  BASE_API: '"http://mall-admin.mall-k8s.com"'
 })
